81f931493563ef1c926c7b5ab89d328bceb1b797
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-12-02 19:45:48 +0100

    feat(SPA): start spa development

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,5 +1,24 @@
 import { h, render } from 'preact';
+import { Link } from 'preact-router';
+import Conference from './pages/conference';
+import Home from './pages/home';
 
-const app = h('h1', null, 'Hello World');
+function App() {
+    return (
+        <div>
+            <header>
+                <Link href="/">Home</Link>
+                <br />
+                <Link href="/conference/amsterdam2019">Amsterdam 2019</Link>
+            </header>
 
-render(app, document.getElementById('app'));
+            <Router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </Router>
+        </div>
+    )
+}
+
+
+render(App, document.getElementById('app'));
